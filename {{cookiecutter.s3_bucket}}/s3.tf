53c18ffee9966f1ff4e4bb47f5acf85f0aaf690a
nocode99 <nocode99@gmail.com>
2019-08-12 21:37:45 -0400

    fixed typo

--- {{cookiecutter.s3_bucket}}/s3.tf
+++ {{cookiecutter.s3_bucket}}/s3.tf
@@ -4,6 +4,6 @@ resource "aws_s3_bucket" "{{cookiecutter.s3_bucket}}" {
 
 {% if cookiecutter.cross_region_replication %}
 resource "aws_s3_bucket" "{{cookiecutter.s3_bucket}}_crr" {
-  provider = "aws.{{var.region_destination}}"
+  provider = "aws.${var.region_destination}"
 }
 {% endif %}
