ca41397e5e0e7efd9ae2ceb809c79b35696c9119
nocode99 <nocode99@gmail.com>
2019-08-12 21:43:58 -0400

    fixed for better tf standards

--- {{cookiecutter.s3_bucket}}/kms.tf
+++ {{cookiecutter.s3_bucket}}/kms.tf
@@ -1,9 +1,9 @@
 {% if cookiecutter.kms_enabled %}
-resource "aws_kms_key" "{{cookiecutter.s3_bucket}}" {
+resource "aws_kms_key" "{{cookiecutter.s3_bucket_tf}}" {
   description = "KMS key for {{cookiecutter.s3_bucket}}"
 }
 
-resource "aws_kms_key_alias "{{cookiecutter.s3_bucket}} {
+resource "aws_kms_key_alias" "{{cookiecutter.s3_bucket_tf}}" {
   name          = "alias/{{cookiecutter.s3_bucket}}"
   target_key_id = "${aws_kms_key.{{cookiecutter.s3_bucket}}.key_id}"
 }
--- {{cookiecutter.s3_bucket}}/s3.tf
+++ {{cookiecutter.s3_bucket}}/s3.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "{{cookiecutter.s3_bucket}}" {
+resource "aws_s3_bucket" "{{cookiecutter.s3_bucket_tf}}" {
   bucket = "{{cookiecutter.s3_bucket}}"
 }
 
 {% if cookiecutter.cross_region_replication %}
-resource "aws_s3_bucket" "{{cookiecutter.s3_bucket}}_crr" {
+resource "aws_s3_bucket" "{{cookiecutter.s3_bucket_tf}}_crr" {
   provider = "aws.${var.region_destination}"
 }
 {% endif %}
